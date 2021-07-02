ecd102c16800ec97bc31aabe40abccdb50233fe8
Luuuuuis <45736619+Luuuuuis@users.noreply.github.com>
2021-07-02 22:53:45 +0200

    fixed padding

--- view/src/views/Home.vue
+++ view/src/views/Home.vue
@@ -4,32 +4,32 @@
       <div class="flex flex-col min-h-screen py-8">
         <div class="flex-none">
           <div class="w-20 h-20 p-4 bg-white rounded-lg shadow-xl">
-            <img src="../assets/logo.png" alt="logo">
+            <img alt="logo" src="../assets/logo.png">
             <!-- WIR BRAUCHEN UNBEDINGT EIN QUADRATISCHES LOGO -->
           </div>
         </div>
 
         <div class="flex-grow">
-          <div class="flex space-x-6 items-center">
+          <div class="pt-24 md:pt-36 lg:pt-52 xl:pt-72 2xl:pt-80 grid grid-cols-2">
             <div class="">
               <div class="space-y-3">
                 <div class="font-medium text-7xl">
-                  <p>Nur für Android! Fu iOS</p>
+                  <p>Nur für Android! yeyeye Was geht break</p>
                 </div>
 
                 <div class="w-64 h-64 badge">
                   <a href='https://play.google.com/store/apps/details?id=de.effnerapp.effner'>
-                    <img alt='Jetzt bei Google Play'
+                    <img alt='Jetzt bei Google Play' class="shad"
                          src='https://play.google.com/intl/en_us/badges/static/images/badges/de_badge_web_generic.png'/>
                   </a>
                 </div>
               </div>
             </div>
 
-            <div class="``">
-              <div class="mx-auto">
+            <div class="">
+              <div class="flex justify-center">
                 <div>
-                  asdasd
+                  Hier coole bilder einfügen
                 </div>
               </div>
             </div>
@@ -37,7 +37,7 @@
         </div>
 
         <div class="flex-none">
-          <div>
+          <div class="flex justify-center">
             Footer here
           </div>
         </div>
@@ -54,15 +54,11 @@ export default {
 </script>
 
 <style scoped>
-.box {
-  margin: 0;
-  position: absolute;
-  top: 50%;
-  -ms-transform: translateY(-50%);
-  transform: translateY(-50%);
-}
-
 .badge {
   transform: translateX(-1rem);
+}
+
+.shad {
+  filter: drop-shadow(0 0 0.5rem rgba(0, 0, 0, 0.4));
 }
 </style>
